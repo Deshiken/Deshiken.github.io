b078294a59bc581aee867e9176fe5038eeb043de
Troy <twthomas1@cougars.ccis.edu>
2024-10-14 07:01:25 -0500

    working prototype of dice roller

--- src/app/board-game-tools/dice-roller/dice-roller.component.ts
+++ src/app/board-game-tools/dice-roller/dice-roller.component.ts
@@ -9,23 +9,119 @@ import DiceBox from '@3d-dice/dice-box'
 export class DiceRollerComponent implements OnInit {
 
   // @ViewChild('diceContainer', { static: true }) diceContainer!: ElementRef;
-
+  diceBox: any;
+  DiceType = DiceType;
+  diceToRoll: Array<DiceGroup> = [
+    {numberOfDice: 0, diceType: DiceType.D4},
+    {numberOfDice: 2, diceType: DiceType.D6},
+    {numberOfDice: 0, diceType: DiceType.D8},
+    {numberOfDice: 0, diceType: DiceType.D10},
+    {numberOfDice: 2, diceType: DiceType.D12},
+    {numberOfDice: 1, diceType: DiceType.D20}
+  ]
+  
   ngOnInit(): void {
-    const diceBox = new DiceBox({
+    this.diceBox = new DiceBox({
       assetPath: '/assets/dice-box/',
-      scale: 7,
+      scale: 5,
       container: '#dice-container',
     });
 
-    diceBox.init().then(() => {
-      console.log('dice box init')
-      // let element: HTMLCanvasElement = document.getElementsByClassName('dice-box-canvas')[0] as HTMLCanvasElement;
-      // element.classList.add('dice-box-canvas-size');
-      // element.style.height = '100%'
-      
-      diceBox.roll('2d20')
-    })
-
-
+    this.diceBox.init()
   }
+
+  rollDice(): void {
+    // this.diceBox.init().then(() => {
+    //   this.diceBox.OnRollComplete = (rollResult: any) => console.log('roll result: ', rollResult);
+
+    //   this.diceBox.roll(this.diceGroupToStringArray()).then((results: any) => {
+    //     console.log('roll results:', results);
+    //   });
+    // })
+    this.diceBox.roll(this.diceGroupToStringArray()).then((results: any) => {
+      console.log('roll results:', results);
+    });
+  }
+
+  diceGroupToStringArray(): Array<string> {
+    let diceToRollArray = new Array<string>();
+    this.diceToRoll.forEach((diceGroup: DiceGroup) => {
+      if (diceGroup.numberOfDice > 0) {
+        diceToRollArray.push(diceGroup.numberOfDice.toString() + DiceType[diceGroup.diceType as keyof typeof DiceType].toLowerCase() )
+      }
+    })
+    console.log('ending dice to roll array: ', diceToRollArray);
+    return diceToRollArray;
+  }
+
+  modifyNumberOfDiceToRoll(diceType: DiceType, modification: string) {
+    switch(diceType) {
+      case DiceType.D4:
+        this.diceToRoll[0].numberOfDice ++;
+        break;
+      case DiceType.D6:
+        this.diceToRoll[1].numberOfDice ++;
+        break;
+      case DiceType.D8:
+        this.diceToRoll[2].numberOfDice ++;
+        break;
+      case DiceType.D10:
+        this.diceToRoll[3].numberOfDice ++;
+        break;
+      case DiceType.D12:
+        this.diceToRoll[4].numberOfDice ++;
+        break;
+      case DiceType.D20:
+        this.diceToRoll[5].numberOfDice ++;
+        break;
+    }
+  }
+
+  increaseDiceGroupeCount(diceGroup: DiceGroup) {
+    diceGroup.numberOfDice ++;
+  }
+  
+  decreaseDiceGroupeCount(diceGroup: DiceGroup) {
+    if (diceGroup.numberOfDice > 0) {
+      diceGroup.numberOfDice --;
+    }
+  }
+  
+  removeDieToRoll(diceType: DiceType) {
+    switch(diceType) {
+      case DiceType.D4:
+        this.diceToRoll[0].numberOfDice --;
+        break;
+      case DiceType.D6:
+        this.diceToRoll[1].numberOfDice --;
+        break;
+      case DiceType.D8:
+        this.diceToRoll[2].numberOfDice --;
+        break;
+      case DiceType.D10:
+        this.diceToRoll[3].numberOfDice --;
+        break;
+      case DiceType.D12:
+        this.diceToRoll[4].numberOfDice --;
+        break;
+      case DiceType.D20:
+        this.diceToRoll[5].numberOfDice --;
+        break;
+    }
+  }
+
+}
+
+interface DiceGroup {
+  numberOfDice: number,
+  diceType: DiceType
+}
+
+enum DiceType {
+  D4 = 'D4',
+  D6 = 'D6',
+  D8 = 'D8',
+  D10 = 'D10',
+  D12 = 'D12',
+  D20 = 'D20', 
 }
