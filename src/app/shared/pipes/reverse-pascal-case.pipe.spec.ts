b0eae29159529af243b97e5bd33832ade6f3c3ad
TSO7099 <troythomas@shelterinsurance.com>
2026-05-20 14:02:49 -0500

    add tests for reverse pascal pipe

--- src/app/shared/pipes/reverse-pascal-case.pipe.spec.ts
+++ src/app/shared/pipes/reverse-pascal-case.pipe.spec.ts
@@ -1,8 +1,21 @@
+import { TestBed } from '@angular/core/testing';
 import { ReversePascalCasePipe } from './reverse-pascal-case.pipe';
 
 describe('ReversePascalCasePipe', () => {
   it('create an instance', () => {
     const pipe = new ReversePascalCasePipe();
     expect(pipe).toBeTruthy();
+  });
+  it('should reverse PascalCase to Pascal Case', () => {
+    const pipe = new ReversePascalCasePipe();
+    expect(pipe.transform('PascalCase')).toBe('Pascal Case');
+  });
+  it('should reverse Pascal_Case to Pascal_ Case', () => {
+    const pipe = new ReversePascalCasePipe();
+    expect(pipe.transform('Pascal_Case')).toBe('Pascal_ Case');
+  });
+  it('should handle empty string', () => {
+    const pipe = new ReversePascalCasePipe();
+    expect(pipe.transform('')).toBe('');
   });
 });
